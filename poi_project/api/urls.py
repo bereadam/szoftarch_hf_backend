11b636946a5ac2a03685118654b66aca65a8b1d2
bereadam <bere.adam15@gmail.com>
2017-11-16 11:08:46 +0100

    authenticetion (register, login, logout)

--- poi_project/api/urls.py
+++ poi_project/api/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, include
 from rest_framework import routers
 from api.views.category import CategoryViewSet, SubcategoryView, AddPoiView
 from api.views.poi import PoiViewSet
+from api.views.auth import LoginView, LogoutView, RegisterView
 
 router = routers.DefaultRouter()
 router.register(r'category', CategoryViewSet)
@@ -10,5 +11,8 @@ router.register(r'poi', PoiViewSet)
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'addSubcategory$', SubcategoryView.as_view()),
-    url(r'addPoi', AddPoiView.as_view())
+    url(r'addPoi', AddPoiView.as_view()),
+    url(r'register', RegisterView.as_view()),
+    url(r'logout', LogoutView.as_view()),
+    url(r'login', LoginView.as_view()),
 ]
---
+++ poi_project/api/views/auth.py
@@ -0,0 +1,56 @@
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from rest_framework import permissions
+from django.http.response import HttpResponse, JsonResponse
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+
+
+class RegisterView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+
+        try:
+            email = request.data['email']
+            password = request.data['password']
+        except KeyError as e:
+            return HttpResponse(str(e.args[0]) + 'is missing!', status=400)
+
+        new_user = User()
+
+        new_user.username = email
+        new_user.set_password(password)
+        new_user.save()
+
+        token = Token.objects.create(user=new_user)
+
+        return HttpResponse(token)
+
+
+class LoginView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+
+        try:
+            email = request.data['email']
+            password = request.data['password']
+        except KeyError as e:
+            return HttpResponse(str(e.args[0]) + 'is missing!', status=400)
+
+        user = authenticate(request, username=email, password=password)
+
+        if user is not None:
+            token = Token.objects.get_or_create(user=user)[0]
+            return JsonResponse({"sessionID": str(token)})
+
+        return JsonResponse({"Error": "unauthorized"})
+
+
+class LogoutView(APIView):
+    queryset = User.objects.all()
+
+    def get(self, request):
+        request.user.auth_token.delete()
+        return HttpResponse()
